6651734943ef0073654aa62e279dc93b54f19209
frontendgod <3054750147@qq.com>
2020-04-05 15:42:07 +0800

    04-05

--- index.js
+++ index.js
@@ -4,27 +4,28 @@ const app = new Koa()
 const router = new Router()
 const usersRouter = new Router({prefix: '/users'})
 
-const auth = async (ctx, next) => {
-    if (ctx.url !== '/users') {
-       ctx.throw(401) 
-    }
-    await next()
-}
-
 router.get('/', () => {
     ctx.body = '这是主页'
 })
 
-router.get('/users', (ctx) => {
-    ctx.body = '这是用户列表'
+usersRouter.get('/', (ctx) => {
+    ctx.body = [{name: '李雷'}, {name: '韩梅梅'}]
 })
 
-usersRouter.post('/', auth, (ctx) => {
+usersRouter.post('/', (ctx) => {
     ctx.body = '创建用户'
 })
 
-usersRouter.get('/:id', auth, (ctx) => {
-    ctx.body = `这是用户 ${ctx.params.id}`
+usersRouter.get('/:id', (ctx) => {
+    ctx.body = [{name: '李雷'}]
+})
+
+usersRouter.put('/:id', (ctx) => {
+    ctx.body = [{name: '李雷2'}]
+})
+
+usersRouter.delete('/:id', (ctx) => {
+    ctx.status = 204
 })
 
 app.use(router.routes())
