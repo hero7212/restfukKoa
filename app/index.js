1c3722c0a33e832c65e4d9d8d8a83315db4a9bf1
frontendgod <3054750147@qq.com>
2020-04-05 20:16:13 +0800

    05-04

---
+++ app/index.js
@@ -0,0 +1,9 @@
+const Koa = require('koa')
+const bodyparser = require('koa-bodyparser')
+const app = new Koa()
+const routing = require('./routes')
+
+app.use(bodyparser())
+routing(app)
+
+app.listen(3000, () => console.log('程序启动在 3000 端口了'))
