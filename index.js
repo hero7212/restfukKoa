59c237ec6e4369624996c927afa9dfce93ea958d
frontendgod <3054750147@qq.com>
2020-04-05 13:30:01 +0800

    04-03

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 const Koa = require('koa')
+const Router = require('koa-router')
 const app = new Koa()
+const router = new Router()
+const usersRouter = new Router({prefix: '/users'})
 
-app.use(async (ctx, next) => {
-    console.log(1)
+const auth = async (ctx, next) => {
+    if (ctx.url !== '/users') {
+       ctx.throw(401) 
+    }
     await next()
-    console.log(2)
-    ctx.body = 'Hello Zhihu API'
+}
+
+router.get('/', () => {
+    ctx.body = '这是主页'
 })
-app.use(async (ctx, next) => {
-    console.log(3)
-    await next()
-    console.log(4)
+
+router.get('/users', (ctx) => {
+    ctx.body = '这是用户列表'
 })
-app.use(async (ctx, next) => {
-    console.log(5)
+
+usersRouter.post('/', auth, (ctx) => {
+    ctx.body = '创建用户'
 })
+
+usersRouter.get('/:id', auth, (ctx) => {
+    ctx.body = `这是用户 ${ctx.params.id}`
+})
+
+app.use(router.routes())
+app.use(usersRouter.routes())
 
 app.listen(3000)
