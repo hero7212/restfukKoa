748e108c3a60c2b649a19139d7f39c1401705525
frontendgod <3054750147@qq.com>
2020-04-05 22:21:46 +0800

    06-05

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -11,10 +11,18 @@ class UsersCtl {
         ctx.body = db[ctx.params.id * 1]
     }
     create(ctx) { 
+        ctx.verifyParams({
+            name: {type: 'string', required: true},
+            age: {type: 'number', required: false}
+        })
         db.push(ctx.request.body)
         ctx.body = ctx.request.body
     }
     update(ctx) { 
+        ctx.verifyParams({
+            name: {type: 'string', required: true},
+            age: {type: 'number', required: false}
+        })
         db[ctx.params.id * 1] = ctx.request.body
         ctx.body = ctx.request.body
     }
