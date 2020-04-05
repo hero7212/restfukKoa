12a946741bfbda051205cbc1489aada5a6bfe229
frontendgod <3054750147@qq.com>
2020-04-05 20:45:53 +0800

    06-02

--- app/controllers/users.js
+++ app/controllers/users.js
@@ -1,9 +1,13 @@
 const db = [{name: '李雷'}]
 class UsersCtl {
     find(ctx) { 
+        
         ctx.body = db
      }
     findById(ctx) {  
+        if (ctx.params.id * 1 >= db.length) {
+            ctx.throw(412, '先决条件失败：id大于等于数组长度了')
+        }
         ctx.body = db[ctx.params.id * 1]
     }
     create(ctx) { 
