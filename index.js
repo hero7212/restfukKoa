a13963f3aa8fe337295bf618ff4f5c6bfdf9371e
frontendgod <3054750147@qq.com>
2020-04-05 18:42:13 +0800

    05-03

--- index.js
+++ index.js
@@ -5,27 +5,32 @@ const app = new Koa()
 const router = new Router()
 const usersRouter = new Router({prefix: '/users'})
 
+const db = [{name: '李雷'}]
+
 router.get('/', (ctx) => {
-    ctx.body = '这是主页'
+    ctx.body = `<h1>主页</h1>`
 })
 
 usersRouter.get('/', (ctx) => {
-    ctx.body = [{name: '李雷'}, {name: '韩梅梅'}]
+    ctx.body = db
 })
 
 usersRouter.post('/', (ctx) => {
-    ctx.body = {name: '李雷'}
+    db.push(ctx.request.body)
+    ctx.body = ctx.request.body
 })
 
 usersRouter.get('/:id', (ctx) => {
-    ctx.body = [{name: '李雷'}]
+    ctx.body = db[ctx.params.id * 1]
 })
 
 usersRouter.put('/:id', (ctx) => {
-    ctx.body = [{name: '李雷2'}]
+    db[ctx.params.id * 1] = ctx.request.body
+    ctx.body = ctx.request.body
 })
 
 usersRouter.delete('/:id', (ctx) => {
+    db.splice(ctx.params.id * 1, 1)
     ctx.status = 204
 })
 
