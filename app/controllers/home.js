c1225f5a0eb773745774af82dd9152dbd2e0ba3c
frontendgod <3054750147@qq.com>
2020-04-11 15:26:35 +0800

    09-02 done

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -2,6 +2,10 @@ class HomeCtl {
     index (ctx) {
         ctx.body = `<h1>主页</h1>`
     }
+    upload (ctx) {
+        const file = ctx.request.files.file
+        ctx.body = { path: file.path }
+    }
 }
 
 module.exports = new HomeCtl()
