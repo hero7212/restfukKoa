ca466f615a8b5251a631a12adf75736af95145d7
frontendgod <3054750147@qq.com>
2020-04-11 11:13:43 +0800

    08-05 done

--- app/models/users.js
+++ app/models/users.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose')
 const { Schema, model } = mongoose
 
 const userSchema = new Schema({
+    __v: {type: Number, select: false},
     name: { type: String, require: true },
+    password: { type: String, require: true, select: false }
 })
 
 module.exports = model('User', userSchema)
