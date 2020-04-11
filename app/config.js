c95f4fa5e4ae7969725d98f39fc90df229c9f74b
frontendgod <3054750147@qq.com>
2020-04-11 12:28:20 +0800

    08-06 done

--- app/config.js
+++ app/config.js
@@ -1,3 +1,4 @@
 module.exports = {
+    secret: 'zhihu-jwt-secret',
     connectionStr: 'mongodb+srv://labnize:ilovejuly@zhihu-ykpax.mongodb.net/test?retryWrites=true&w=majority'
 }
--- app/routes/users.js
+++ app/routes/users.js
@@ -1,6 +1,9 @@
 const Router = require('koa-router')
 const router = new Router({prefix: '/users'})
-const { find, findById, create, update, delete: del } = require('../controllers/users')
+const { 
+    find, findById, create, update, 
+    delete: del, login 
+} = require('../controllers/users')
 
 
 router.get('/', find)
@@ -12,5 +15,7 @@ router.get('/:id', findById)
 router.patch('/:id', update)
 
 router.delete('/:id', del)
+
+router.post('/login', login)
 
 module.exports = router
