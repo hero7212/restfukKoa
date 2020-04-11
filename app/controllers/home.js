2b7649d6261deda36aa3afcd8b9e10ee048a944a
frontendgod <3054750147@qq.com>
2020-04-11 16:01:37 +0800

    09-03 done

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,10 +1,12 @@
+const path = require('path')
 class HomeCtl {
     index (ctx) {
         ctx.body = `<h1>主页</h1>`
     }
     upload (ctx) {
         const file = ctx.request.files.file
-        ctx.body = { path: file.path }
+        const basename = path.basename(file.path)
+        ctx.body = { url: `${ctx.origin}/uploads/${basename}` }
     }
 }
 
--- app/index.js
+++ app/index.js
@@ -1,5 +1,6 @@
 const Koa = require('koa')
 const koaBody = require('koa-body')
+const koaStatic = require('koa-static')
 const error = require('koa-json-error')
 const parameter = require('koa-parameter')
 const mongoose = require('mongoose')
@@ -11,7 +12,7 @@ const { connectionStr } = require('./config')
 mongoose.connect(connectionStr, { useNewUrlParser: true,useUnifiedTopology: true }, () => console.log('连接成功'))
 mongoose.connection.on('error', console.error)
 
-
+app.use(koaStatic(path.join(__dirname, 'public')))
 app.use(error({
     postFormat: (e, {stack, ...rest}) => process.env.NODE_ENV === 'production' ? rest: {stack, ...rest}
 }))
