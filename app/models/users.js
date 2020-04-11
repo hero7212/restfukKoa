2b604465e00ccde7fd8c8f4ae9d095a708718f7b
frontendgod <3054750147@qq.com>
2020-04-11 18:00:10 +0800

    10-02 done

--- app/models/users.js
+++ app/models/users.js
@@ -5,7 +5,27 @@ const { Schema, model } = mongoose
 const userSchema = new Schema({
     __v: {type: Number, select: false},
     name: { type: String, require: true },
-    password: { type: String, require: true, select: false }
+    password: { type: String, require: true, select: false },
+    avatar_url: { type: String },
+    gender: { type: String, enum: ['male', 'female'], default: 'male', required: true },
+    headline: { type: String },
+    locations: { type: [{ type: String }] },
+    businness: { type: String },
+    employments: {
+        type: [{
+            company: { type: String },
+            job: { type: String }
+        }],
+    },
+    educations: {
+        type: [{
+            school: { type: String },
+            major: { type: String },
+            diploma: { type: Number, enum: [1, 2, 3, 4, 5] },
+            entrance_year: { type: Number },
+            graduation_year: { type: Number }
+        }]
+    }
 })
 
 module.exports = model('User', userSchema)
