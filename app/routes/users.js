8e9e49227c9ef66638f03256875150578c882b90
frontendgod <3054750147@qq.com>
2020-04-18 11:46:23 +0800

    11-04

--- app/routes/users.js
+++ app/routes/users.js
@@ -4,7 +4,7 @@ const router = new Router({prefix: '/users'})
 const { 
     find, findById, create, update, 
     delete: del, login, checkOwner,
-    listFollowing
+    listFollowing, listFollowers, follow, unfollow
 } = require('../controllers/users')
 
 const { secret } = require('../config')
@@ -12,19 +12,14 @@ const { secret } = require('../config')
 const auth = jwt({ secret })
 
 router.get('/', find)
-
 router.post('/', create)
-
 router.get('/:id', findById)
-
 router.patch('/:id', auth, checkOwner, update)
-
 router.delete('/:id', auth, checkOwner, del)
-
 router.post('/login', login)
-
 router.get('/:id/following', listFollowing)
-
-router.put('/following/:id', auth)
+router.get('/:id/followers', listFollowers)
+router.put('/following/:id', auth, follow)
+router.delete('/following/:id', auth, unfollow)
 
 module.exports = router
